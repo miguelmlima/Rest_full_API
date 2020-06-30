bc77de52420d9176adfb1be38f1518c5c8c5959d
Miguel <miguelmoreiralima@outlook.com>
2020-06-30 20:34:25 -0300

    Implements endppoints get

---
+++ src/main/java/br/com/resfull/service/PersonService.java
@@ -0,0 +1,45 @@
+package br.com.resfull.service;
+
+import br.com.resfull.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class PersonService {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    public Person findById(String id){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Eduardo");
+        person.setLastName("Souza");
+        person.setAddress("Campinas - São Paulo - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 9; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+        
+        
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person Name" + i);
+        person.setLastName("Last Name" + i);
+        person.setAddress("Some address in Brazil" + i);
+        person.setGender("Male");
+        return person;
+    }
+}
---
+++ src/test/java/br/com/resfull/ResfullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.resfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
