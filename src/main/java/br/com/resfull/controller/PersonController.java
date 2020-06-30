5a322a794c21f9ea03c675b6d8d6307c0a19432e
Miguel <miguelmoreiralima@outlook.com>
2020-06-30 20:51:16 -0300

    implements post, put and delete

--- src/main/java/br/com/resfull/controller/PersonController.java
+++ src/main/java/br/com/resfull/controller/PersonController.java
@@ -4,10 +4,7 @@ import br.com.resfull.model.Person;
 import br.com.resfull.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,17 @@ public class PersonController {
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return service.findAll();
+    }
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+    @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+         service.delete(id);
     }
 }
