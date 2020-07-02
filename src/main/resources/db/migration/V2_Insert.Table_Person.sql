c954901917819b1aee205e936a147ebea4f23ed9
Miguel <miguelmoreiralima@outlook.com>
2020-07-01 21:17:07 -0300

    config flyway migrate

---
+++ src/main/resources/db/migration/V2_Insert.Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `las_name`) VALUES
+        (1,'Campinas Brasil','Leonardo','Male','Oliveira'),
+        (2,'São Bernardo do Campo Brasil','Bianca','Female','Oliveira'),
+        (5,'Campinas Brasil','Leonardo','Male','Santos Silva'),
+        (6,'Campinas - São Paulo - Brasil','André','Male','Souza');
