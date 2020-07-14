f40df9fb42c47756b4478718fea9891ec370f924
Miguel <miguelmoreiralima@outlook.com>
2020-07-14 18:13:27 -0300

    add swagger

--- src/main/java/br/com/restfull/config/SwaggerConfig.java
+++ src/main/java/br/com/restfull/config/SwaggerConfig.java
@@ -1,31 +1,57 @@
 package br.com.restfull.config;
 
 
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("br.com.restfull"))
-//                .paths(PathSelectors.any())
-//                .build();
-//
-//    }
-//
-//    private ApiInfo metaInfo() {
-//        ApiInfo apiInfo = new ApiInfo(
-//                "Cartões API Rest",
-//                "API Rest cadastro de Cartões",
-//                "1.0",
-//                "Terms of Service",
-//                new Contact("Miguel Lima", "https://www.linkedin.com/in/miguel-moreira-lima-11813ab7/",
-//                        "miguelmoreiralima@outlook.com"),
-//                "Apache License Version 2.0",
-//                "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
-//        );
-//        return apiInfo;
-//    }
-//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Primary
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.restfull"))
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Cartões API Rest",
+                "API Rest cadastro de Cartões",
+                "1.0",
+                "Terms of Service",
+                new Contact("Miguel Lima", "https://www.linkedin.com/in/miguel-moreira-lima-11813ab7/",
+                        "miguelmoreiralima@outlook.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
+        );
+        return apiInfo;
+    }
+}
