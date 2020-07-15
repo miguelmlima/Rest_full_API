2b1d1f4b491d5719ea2fdbf3fea36de8acc53124
Miguel <miguelmoreiralima@outlook.com>
2020-07-15 20:29:01 -0300

    authentication implementation

---
+++ src/main/java/br/com/restfull/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package br.com.restfull.config;
+
+
+import br.com.restfull.security.jwt.JwtConfigurer;
+import br.com.restfull.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder () {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/auth/sign", "/api-docs**", "swagger-ui.html***").permitAll()
+                    .antMatchers("/api/**").authenticated()
+                    .antMatchers("/users").denyAll()
+                .and()
+                .apply(new JwtConfigurer(tokenProvider));
+    }
+
+}
---
+++ src/main/java/br/com/restfull/controller/AuthController.java
@@ -0,0 +1,67 @@
+package br.com.restfull.controller;
+
+import br.com.restfull.data.dto.AccountCredentialsDTO;
+import br.com.restfull.data.model.User;
+import br.com.restfull.repository.UserRepository;
+import br.com.restfull.security.jwt.JwtTokenProvider;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenProvider tokenProvider;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @PostMapping(value = "/signin", produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml", "application/x-yaml"})
+    @ApiOperation(value = "Authentication a user by credentials")
+    public ResponseEntity sigin(@RequestBody AccountCredentialsDTO data) {
+        try {
+            String username = data.getUsername();
+            String password = data.getPassword();
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            User user = userRepository.findByUsername(username);
+
+            String token = "";
+
+            if (user != null) {
+                token = tokenProvider.createToken(username, user.getRoles());
+            } else {
+                throw new UsernameNotFoundException("Username " + username + " not found");
+            }
+
+            Map<Object, Object> model = new HashMap<>();
+            model.put("username", username);
+            model.put("token", token);
+            return ok(model);
+
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username/password supplied!");
+        }
+    }
+}
